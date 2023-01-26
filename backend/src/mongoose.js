7de733f2247952529f4fa1bc81bccf268ec88254
Robin Caramaschi <caramaschi@hotmail.com>
2023-01-26 19:21:38 +0100

    dockerisation of the app

--- backend/src/mongoose.js
+++ backend/src/mongoose.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 module.exports = function (app) {
+  const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+  } = process.env;
   mongoose.connect(
-    app.get('mongodb')
+     `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+
   ).catch(err => {
     logger.error(err);
     process.exit(1);
